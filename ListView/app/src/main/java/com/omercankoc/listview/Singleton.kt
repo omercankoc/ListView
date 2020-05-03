a1b13b8af956f148e59bb979cb7532135064b765
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-05-03 15:42:37 +0300

    .
    .

--- ListView/app/src/main/java/com/omercankoc/listview/Singleton.kt
+++ ListView/app/src/main/java/com/omercankoc/listview/Singleton.kt
@@ -3,7 +3,7 @@ package com.omercankoc.listview
 import android.graphics.Bitmap
 
 class Singleton {
-    // Sinifin tekilligini (Singleton) saglamak ve static nesne ve
+    // Sinifin tekilligini (Singleton) saglamak, static nesne ve
     // metotlarin tanimlanmasi icin companion object blogu kullanilir.
     companion object Selected {
         var selectedImage : Bitmap? = null
